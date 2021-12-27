04064ea21dc7398e8db04249274a274b6bc2a6b5
Roopesh Chander <roop@roopc.net>
2021-12-27 13:50:41 +0530

    iOS: AppDelegate: Configure and start the tunnel

--- iOS/UsingWireGuardKit/UsingWireGuardKit/AppDelegate.swift
+++ iOS/UsingWireGuardKit/UsingWireGuardKit/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import NetworkExtension
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        turnOnTunnel { isSuccess in
+            if isSuccess {
+                NSLog("Starting tunnel succeeded")
+                NSLog("Will turn off in 20 seconds")
+                DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(20)) { [weak self] in
+                    self?.turnOffTunnel()
+                }
+            }
+        }
         return true
     }
 
@@ -34,3 +44,97 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate {
+
+    // Assumptions:
+    //   - We just need one tunnel in iOS Settings / macOS Preferences for the app.
+    //     We can reuse that with different configs if required.
+    //   - The tunnel shall not be started from iOS Settings / macOS Preferences.
+    //     It has to be started using the container app only.
+    //   - The tunnel need not be configured to start automatically under
+    //     certain conditions using on-demand
+
+    func turnOnTunnel(completionHandler: @escaping (Bool) -> Void) {
+        // We use loadAllFromPreferences to see if this app has already added a tunnel
+        // to iOS Settings or (macOS Preferences)
+        NETunnelProviderManager.loadAllFromPreferences { tunnelManagersInSettings, error in
+            if let error = error {
+                NSLog("Error (loadAllFromPreferences): \(error)")
+                completionHandler(false)
+                return
+            }
+
+            // If the app has already added a tunnel to Settings, we are going to modify that.
+            // If not, we create a new instance and save that to Settings.
+            // We will always have either 0 or 1 tunnel only in Settings for this app.
+            let preExistingTunnelManager = tunnelManagersInSettings?.first
+            let tunnelManager = preExistingTunnelManager ?? NETunnelProviderManager()
+
+            // Configure the custom VPN protocol
+            let protocolConfiguration = NETunnelProviderProtocol()
+
+            // Set the tunnel extension's bundle id
+            protocolConfiguration.providerBundleIdentifier = "<bundle-id-of-tunnel-extension>"
+
+            // Set the server address as a non-nil string.
+            // It would be good to provide the server's domain name or IP address.
+            protocolConfiguration.serverAddress = "server"
+
+            tunnelManager.protocolConfiguration = protocolConfiguration
+            tunnelManager.isEnabled = true
+
+            // Save the tunnel to preferences.
+            // This would modify the existing tunnel, or create a new one.
+            tunnelManager.saveToPreferences { error in
+                if let error = error {
+                    NSLog("Error (saveToPreferences): \(error)")
+                    completionHandler(false)
+                    return
+                }
+                // Load it back so we have a valid usable instance.
+                tunnelManager.loadFromPreferences { error in
+                    if let error = error {
+                        NSLog("Error (loadFromPreferences): \(error)")
+                        completionHandler(false)
+                        return
+                    }
+
+                    // At this point, the tunnel is configured.
+                    // Attempt to start the tunnel
+                    do {
+                        NSLog("Starting the tunnel")
+                        guard let session = tunnelManager.connection as? NETunnelProviderSession else {
+                            fatalError("tunnelManager.connection is invalid")
+                        }
+                        try session.startTunnel()
+                    } catch {
+                        NSLog("Error (startTunnel): \(error)")
+                        completionHandler(false)
+                    }
+                    completionHandler(true)
+                }
+            }
+        }
+    }
+
+    func turnOffTunnel() {
+        NETunnelProviderManager.loadAllFromPreferences { tunnelManagersInSettings, error in
+            if let error = error {
+                NSLog("Error (loadAllFromPreferences): \(error)")
+                return
+            }
+            if let tunnelManager = tunnelManagersInSettings?.first {
+                guard let session = tunnelManager.connection as? NETunnelProviderSession else {
+                    fatalError("tunnelManager.connection is invalid")
+                }
+                switch session.status {
+                case .connected, .connecting, .reasserting:
+                    NSLog("Stopping the tunnel")
+                    session.stopTunnel()
+                default:
+                    break
+                }
+            }
+        }
+    }
+}
